8b23dee1d5d7961a61d92acbe03ad3fc50bd8fa6
Muhammad Usman <muusman479@gmail.com>
2022-11-12 15:26:01 +0500

    post lab 02 added

--- lab_02/lab-exercise/design.v
+++ lab_02/lab-exercise/design.v
@@ -1,19 +1,27 @@
-module ALU(A,B,Q,control);
+module ALU(A,B,result,control,Verflow,Carry,Negative,Zero);
 
     input [31:0]A,B;
     input[2:0]control;
     wire Cout;
-    output [31:0]Q;
-    wire [31:0]A_or_B,A_and_B,B_not,A_sum_B,Ex;
+    output [31:0]result;
+    output Verflow,Carry,Negative,Zero;
+    wire [31:0]A_or_B,A_and_B,B_not,A_sum_B;
+    wire A_xor_sum,A_xnor_B_xnor_control,control_1_not;
     wire [31:0]S1;
 
     assign A_or_B = A | B;
     assign A_and_B = A & B;
     assign B_not = ~B;
     assign S1 = (control[0] == 1'b1) ? B_not : B;
-    assign Ex = {31'b0000000000000000000000000000000,S1[31]};
     assign {Cout,A_sum_B} = A+S1+control[0];
-    assign Q = (control == 3'b000) ? A_sum_B : (control == 3'b001) ? A_sum_B : (control == 3'b010) ? A_and_B : (control == 3'b011) ? A_or_B : Ex;
+    assign A_xor_sum = A[31]^A_sum_B[31];
+    assign A_xnor_B_xnor_control = ~(((A[31]^B[31])^control[0]));
+    assign control_1_not = ~(control[1]);
+    assign Verflow = (A_xnor_B_xnor_control & A_xor_sum) & control_1_not;
+    assign Zero = &(~result);
+    assign Negative = result[31];
+    assign Carry = control_1_not & Cout;
+    assign result = (control[1:0] == 2'b00) ? A_sum_B : (control[1:0] == 2'b01) ? A_sum_B : (control[1:0] == 2'b10) ? A_and_B : A_or_B ;
 
 
 
--- lab_02/lab-exercise/testbench.v
+++ lab_02/lab-exercise/testbench.v
@@ -2,9 +2,10 @@ module tb();
 
     reg [31:0]A,B;
     reg [2:0]control;
-    wire [31:0]Q;
+    wire [31:0]result;
+    wire Verflow,Carry,Negative,Zero;
 
-    ALU dut (.A(A),.B(B),.Q(Q),.control(control));
+    ALU dut (.A(A),.B(B),.result(result),.control(control),.Verflow(Verflow),.Carry(Carry),.Negative(Negative),.Zero(Zero));
 
     initial begin
       $dumpfile("dump.vcd");
@@ -12,17 +13,17 @@ module tb();
     end
 
     initial begin
-        control <= 3'b101;
-        A <= 32'b01000000000000000000000000000000;
-        B <= 32'b01000000000000000000000000000000;
-        #100
         control <= 3'b000;
-        A <= 32'd7;
-        B <= 32'd5;
+        A <= 32'd50;
+        B <= 32'd60; // result 110
         #100
         control <= 3'b001;
-        A <= 32'd7;
-        B <= 32'd5;
+        A <= 32'b00000000000000000000000000111100;
+        B <= 32'b00000000000000000000000000111100; // zero 1
+        #100
+        control <= 3'b000;
+        A <= 32'b10000000000000000000000000000000;
+        B <= 32'b10000000000000000000000000000001; // carry 1 // verflow 1
         #100
 
         $finish;
